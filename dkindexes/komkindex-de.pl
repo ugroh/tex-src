ec08620b6f6f48f39bee7d9052b98931504e64ee
nyamcoder <info@mito-space.com>
2014-03-06 23:43:18 +0100

    first commit

---
+++ dkindexes/komkindex-de.pl
@@ -0,0 +1,20 @@
+﻿#!/usr/bin/env perl
+### Copyrihgt (C) 2004-2007 Dohyun Kim <nomosnomos at gmail.com>
+###
+### -------------------------------------------------------------
+###         mods:  komkindex-de.pl :: mito(XD) / nyamcoder
+###     *** replace the shown lines in komkindex.pl, rename,
+###            save somewhere in $TEXHOME & texhash ***
+### -------------------------------------------------------------
+###
+. . .
+my @hangul_grouping =
+qw/
+F785 F788 F790 F798 F7A8 F7AE F7C2 F7D8
+F7D9 F7E7 F7E8 F7F0 F7F5 F7F6 F7F8 F7FC
+F7FF
+/;
+# ㄱ ㄴ ㄷ ㄹ ㅁ ㅂ ㅅ ㅿ
+# ㅇ ㆁ ㅈ ㅊ ㅋ ㅌ ㅍ ㅎ
+# ㆆ
+. . .
